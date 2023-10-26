4f8c53c6a7f3ae04389c50cff058b8bff4dfdc09
Peter Kurtz <peterjohnkurtz@gmail.com>
2023-10-25 18:02:27 -0600

    Uploaded files.

---
+++ src/Assignment5.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Assignment5 {
+    public static void main(String[] args) {
+        simpleQueueTest();
+        //scheduleTasks("taskset1.txt");
+        //scheduleTasks("taskset2.txt");
+        //scheduleTasks("taskset3.txt");
+        //scheduleTasks("taskset4.txt");
+        //scheduleTasks("taskset5.txt");
+    }
+
+    public static void scheduleTasks(String taskFile) {
+        // TODO: Uncomment code here as you complete the tasks and scheduling algorithm
+//        ArrayList<Task> tasksByDeadline = new ArrayList<>();
+//        ArrayList<Task> tasksByStart = new ArrayList<>();
+//        ArrayList<Task> tasksByDuration = new ArrayList<>();
+//
+//        readTasks(taskFile, tasksByDeadline, tasksByStart, tasksByDuration);
+//
+//        schedule("Deadline Priority : "+ taskFile, tasksByDeadline);
+//        schedule("Startime Priority : " + taskFile, tasksByStart);
+//        schedule("Duration priority : " + taskFile, tasksByDuration);
+    }
+
+    public static void simpleQueueTest() {
+        // TODO: Uncomment code here for a simple test of your priority queue code
+//        PriorityQueue<Integer> queue = new PriorityQueue<>();
+//        queue.enqueue(9);
+//        queue.enqueue(7);
+//        queue.enqueue(5);
+//        queue.enqueue(3);
+//        queue.enqueue(1);
+//        queue.enqueue(10);
+//
+//        while (!queue.isEmpty()) {
+//            System.out.println(queue.dequeue());
+//        }
+    }
+
+    /**
+     * Reads the task data from a file, and creates the three different sets of tasks for each
+     */
+    public static void readTasks(String filename,
+                                 ArrayList<Task> tasksByDeadline,
+                                 ArrayList<Task> tasksByStart,
+                                 ArrayList<Task> tasksByDuration) {
+        // TODO: Write your task reading code here
+    }
+
+    /**
+     * Given a set of tasks, schedules them and reports the scheduling results
+     */
+    public static void schedule(String label, ArrayList<Task> tasks) {
+        // TODO: Write your scheduling algorithm here
+    }
+}
